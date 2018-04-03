09bc7226a0d8a30b5de14c89f9ae358816513b92
chanlo <woluch@foxmail.com>
2018-04-03 23:42:42 +0800

    'update'

---
+++ Crypto.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+'''
+author: ChanLo
+e-mail: chanlo@protonmail.ch
+------------------
+Describtion:
+This is a module used to encrypt or decrypt the password.
+'''
+
+class Crypto(object):
+    def __init__(self):
+        pass
+
+    def encrypt(self, key):
+        pass
+
+    def decrypt(self, key):
+        pass
+    
+    def generate_key():
+        pass
+        
+
+if __name__ == '__main__':
+    crypto = Crypto()
