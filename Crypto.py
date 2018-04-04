07cc8b7a18161a7554bd6368666dc97552b11383
chanlo <woluch@foxmail.com>
2018-04-04 23:38:53 +0800

    'update'

--- Crypto.py
+++ Crypto.py
@@ -18,7 +18,21 @@ class Crypto(object):
         pass
     
     def generate_key():
-        pass
+        import uuid
+        import sys
+        from binascii import unhexlify as unhex
+
+        if sys.platform == 'win32':
+            mac = _ipconfig_getnode()
+        else:
+            mac = _ifconfig_getnode()
+        if mac == None:
+            mac = hex(_random_getnode())[2:-1]
+        ud = uuid.uuid1()
+        ud = ud.hex
+        hi_time = up[12:16]
+        key = hi_time + mac
+        return unhex(key)
         
 
 if __name__ == '__main__':
