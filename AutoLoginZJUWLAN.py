81213db441a45cdf49c88bf3d3f1f76a58834373
chanlo <woluch@foxmail.com>
2018-04-07 23:53:22 +0800

    'update'

--- AutoLoginZJUWLAN.py
+++ AutoLoginZJUWLAN.py
@@ -4,15 +4,40 @@ author: ChanLo
 e-mail: chanlo@protonmail.ch
 '''
 import Detection
+import DataSave
 import urllib
 from urllib.request import urlopen, Request, URLError, HTTPError
 from termcolor import colored, cprint  
 
-nameDB = 'UserDB'
-data = {'action':'login','username':username,'password':password,'ac_id':'3','user_ip':'','nas_ip':'','user_mac':'','save_me':'1','ajax':'1'}
-data = urllib.parse.urlencode(data)
+name_DB = 'UserDB'
+use_account = 0
+is_use_existing_account = True
+aim_id = 1
+
+def if_use_existing_account():
+    while(use_account != 'Y' and use_account !='N'):
+        use_account = input('Use existing account? (Y/N)\n')
+        if use_account == 'Y':
+            return True
+        else use_account == 'N':
+            return False
+
+def get_user_UP():
+    username = input('User name: ')
+    password = input('Password: ')
+    return username, password
 
 def main():
+    print('Hi, there.\n')
+    UPSave = DataSave.UPSave()
+    if not if_use_existing_account():
+        username, password = get_user_UP()
+        UPSave.create_DB(name_DB)
+        UPSave.insert_to_DB(name_DB, username, password)
+    else:
+        username, password = UPSave.select_DB(name_DB, aim_id)
+    data = {'action':'login','username':username,'password':password,'ac_id':'3','user_ip':'','nas_ip':'','user_mac':'','save_me':'1','ajax':'1'}
+    data = urllib.parse.urlencode(data)
     try:
         req = Request('https://net.zju.edu.cn/srun_portal_pc.php?url=&ac_id=3')
         with urlopen(req, data.encode('utf-8'), timeout=10) as response:
@@ -35,7 +60,7 @@ def main():
     except:
         import traceback
         print('Generic exception: ' + traceback.format_exc())
-
+    
 
 if __name__ == '__main__':
     try:
--- DataSave.py
+++ DataSave.py
@@ -19,27 +19,55 @@ class UPSave(object):
             create_sql = '''CREATE TABLE IF NOT EXISTS user
                         (
                             ID INTEGER PRIMARY KEY,
-                            username BLOB NOT NULL UNIQUE ON    \
-                              CONFLICT IGNORE,
-                            password BLOB NOT NULL
+                            username TEXT,
+                            password TEXT
                         );
                         '''
-            cu.execute(create_sql)
+
+            c.execute(create_sql)
             conn.commit()
 
     def insert_to_DB(self, name_DB, username, password):
-        with sqlite3.connect(Name_DB+'.db') as conn:
+        with sqlite3.connect(name_DB+'.db') as conn:
             c = conn.cursor()
-            insert_sql = 'INSERT INTO user (username, password) \
-              VALUES (?,?)', (buffer(username), buffer(password))
-            cu.execute(insert_sql)
+            insert_sql = "INSERT INTO user (username, password) \
+              VALUES ('%s', '%s')" % (username, password)   
+            c.execute(insert_sql)
             conn.commit()
 
     def delete_DB(self, name_DB):
-        with sqlite3.connect as conn:
+        with sqlite3.connect(name_DB+'.db') as conn:
             c = conn.cursor()
             delete_sql = 'DELETE FROM user'
-            cu.execute(delete_sql)
+            c.execute(delete_sql)
             conn.commit()
-    
 
+    def select_DB(self, name_DB, aim_id):
+        with sqlite3.connect(name_DB+'.db') as conn:
+            c = conn.cursor()
+            delete_sql = 'SELECT username,password FROM user WHERE ID=%d' % aim_id
+            cursor = c.execute(delete_sql)
+            for row in cursor:
+                username, password = row
+            conn.commit()
+            return username, password
+    
+    def print_DB(self, name_DB):
+        with sqlite3.connect(name_DB+'.db') as conn:
+            c = conn.cursor()
+            cursor = c.execute('SELECT * FROM user')
+            for row in cursor:
+                print('ID = ', row[0])
+                print('Username = ', row[1])
+                print('Password = ', row[2])
+
+if __name__ == '__main__':
+    UPSave = UPSave()
+    name_DB = 'test'
+    UPSave.create_DB(name_DB)
+    UPSave.insert_to_DB(name_DB, '1a', '123c')
+    UPSave.insert_to_DB(name_DB, '2', '456')
+    UPSave.print_DB(name_DB)
+    username, password = UPSave.select_DB(name_DB, 1)
+    print(username)
+    print(password)
